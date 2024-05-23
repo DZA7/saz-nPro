99c8f6a95fefd3a921d8a8a00bd9622988e5a0e0
Diego Daza <untaldaza77@gmail.com>
2024-05-22 19:17:44 -0500

    primer compromiso

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,6 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <script src="https://cdn.tailwindcss.com"></script>
-@vite('resources/css/app.css')
 <title>sazónPro</title>
 </head>
 <body class="flex flex-col items-center">
